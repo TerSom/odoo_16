92bee4f3e68bcf6fe83957a0e976f6b6b141f8ee
TerSom <ibnua0291@gmail.com>
2025-08-21 09:56:24 +0700

    guide code

--- CustomAddons/Model_test/__manifest__.py
+++ CustomAddons/Model_test/__manifest__.py
@@ -13,13 +13,13 @@
     'application': True,
     # data files always loaded at installation
     'data': [
+        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_offer.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_propery_inherit.xml',
         'views/estate_menus.xml',   
-        'security/ir.model.access.csv',
         
         
     ],
