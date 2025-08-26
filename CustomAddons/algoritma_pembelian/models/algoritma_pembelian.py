77adb350ec7da16e90c3bc5a2422da532c0350be
TerSom <ibnua0291@gmail.com>
2025-08-26 11:49:52 +0700

    update

--- CustomAddons/algoritma_pembelian/models/algoritma_pembelian.py
+++ CustomAddons/algoritma_pembelian/models/algoritma_pembelian.py
@@ -1,8 +1,9 @@
-from odoo import fields,models
+from odoo import fields,models, api
 
 class algoritma_pembelian(models.Model):
     _name = "algoritma.pembelian"
     _description = "algoritma pembelian"
+    _order = 'nama asc'
     
     nama = fields.Char(string="Nama")
     tanggal = fields.Date(string="Tanggal")
@@ -25,8 +26,21 @@ class algoritma_pembelian_line(models.Model):
     product_id = fields.Many2one('product.product',string="product id")
     quantity = fields.Float(string="Quantity", default= 0.0)
     uom_id = fields.Many2one('uom.uom',string="Uom id")
-    
+    name = fields.Char(string="Name")
+    sales_price = fields.Float(string="Sales Price")
+    standar_price = fields.Float(string="Cost")
 
+    @api.onchange('product_id')
+    def _get_name_product_id(self):
+        if not self.product_id:
+            return {}
+        else:
+            self.name = self.product_id.name
+            self.sales_price = self.product_id.list_price
+            self.standar_price = self.product_id.standard_price
+            self.quantity = self.product_id.qty_available
+            # self.uom_id = self.product_id.
+            
 class algoritma_brand(models.Model):
     _name = "algoritma.brand"
     
